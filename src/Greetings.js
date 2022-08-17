7eb0e4d326a21b14207eb636c3a11204f1fc2a7a
GraziosoG <grace.chiu25@utexas.edu>
2022-08-17 10:52:21 +0800

    Update logo

--- src/Greetings.js
+++ src/Greetings.js
@@ -8,7 +8,7 @@ const Greetings = React.forwardRef((props, ref) => {
         <div className="greetings" ref={ref}>
             <h2 className="greetingsTitle"><span>Hi, I'm Grace.</span> <span>How's your day?</span></h2>
             <p className="greetingsIntro">Never Ending Curiousity and Learning, <br></br>Problem Solving, and Creation</p>
-            <span className="greetingsLink"><a href="https://www.linkedin.com/in/gracechiu-ut/" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a><a href="mailto:grace.chiu25@utexas.edu"><FiMail /></a><a href="https://github.com/GraziosoG?tab=repositories" target="_blank" rel="noopener noreferrer"><BsGithub /></a></span>
+            <span className="greetingsLink"><a href="https://www.linkedin.com/in/gracechiu-ut/" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a><a href="mailto:grace.chiu25@utexas.edu"><FiMail /></a><a href="https://github.com/GraziosoG" target="_blank" rel="noopener noreferrer"><BsGithub /></a></span>
         </div>
     )
 })
